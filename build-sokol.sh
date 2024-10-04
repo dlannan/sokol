54494355ef33b5b44936af2acfd04e20a9f77027
David Lannan <dlannan68@gmail.com>
2024-10-04 19:53:33 +0930

    Fixed for aut build.

--- build-sokol.sh
+++ build-sokol.sh
@@ -8,22 +8,22 @@ if [ "${PLATFORM}" = "win64" ]; then
 	cl -c lib/sokol-dll.c
     lib -out:sokol-dll.dll sokol-dll.obj
 elif [ "${PLATFORM}" = "linux" ]; then
-    gcc -c -I./src lib/sokol-dll.c -lpthread -ldl -lm 
+    gcc -c -I./ lib/sokol-dll.c -lpthread -ldl -lm 
     ar rcs sokol-dll.so sokol-dll.o
 elif [ "${PLATFORM}" = "macosx" ]; then
-    gcc -c -I./src lib/sokol-dll.c -lpthread -ldl -lm 
+    gcc -c -I./ lib/sokol-dll.c -lpthread -ldl -lm 
     ar rcs sokol-dll.so sokol-dll.o
 elif [ "${PLATFORM}" = "ios64" ]; then
-    gcc -c -I./srclib/sokol-dll.c -lpthread -ldl -lm -arch arm64
+    gcc -c -I./ lib/sokol-dll.c -lpthread -ldl -lm -arch arm64
     ar rcs sokol-dll.so sokol-dll.o
 elif [ "${PLATFORM}" = "android" ]; then
     export PATH="android-ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH"
     export PATH="android-ndk/toolchains/llvm/prebuilt/linux-x86_64/:$PATH"
     
-    aarch64-linux-android21-clang -c -I./src -Iandroid-ndk/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include lib/sokol-dll.c -D__ANDROID__ -D__ANDROID_MIN_SDK_VERSION__=21 -lpthread -lm -fPIC -static
+    aarch64-linux-android21-clang -c -I./ -Iandroid-ndk/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include lib/sokol-dll.c -D__ANDROID__ -D__ANDROID_MIN_SDK_VERSION__=21 -lpthread -lm -fPIC -static
     llvm-ar rcs sokol-dll-aarch64.so sokol-dll.o
     rm sokol-dll.o
-    armv7a-linux-androideabi19-clang -c -I./src -Iandroid-ndk/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include lib/sokol-dll.c -D__ANDROID__ -D__ANDROID_MIN_SDK_VERSION__=19 -lpthread -lm -fPIC -static
+    armv7a-linux-androideabi19-clang -c -I./ -Iandroid-ndk/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include lib/sokol-dll.c -D__ANDROID__ -D__ANDROID_MIN_SDK_VERSION__=19 -lpthread -lm -fPIC -static
     llvm-ar rcs sokol-dll-armv7.so sokol-dll.o
 fi
 
