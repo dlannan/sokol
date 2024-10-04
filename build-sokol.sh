58458eec426d39cb9e3242e12948ba54e214fb78
David Lannan <dlannan68@gmail.com>
2024-10-04 20:00:10 +0930

    changed build process for shared lib.

--- build-sokol.sh
+++ build-sokol.sh
@@ -5,25 +5,21 @@ PLATFORM=$1
 echo "Building for platform ${PLATFORM}"
 
 if [ "${PLATFORM}" = "win64" ]; then
-	cl -c lib/sokol-dll.c
-    lib -out:sokol-dll.dll sokol-dll.obj
+    cl /D_USRDLL /D_WINDLL sokol-dll.c  /link /DLL /OUT:sokol_dll.dll
 elif [ "${PLATFORM}" = "linux" ]; then
-    gcc -c -I./ lib/sokol-dll.c -lpthread -ldl -lm 
-    ar rcs sokol-dll.so sokol-dll.o
+    gcc -c -I./ lib/sokol-dll.c -o sokol_dll.so sokol-dll.o -lpthread -ldl -lm 
 elif [ "${PLATFORM}" = "macosx" ]; then
-    gcc -c -I./ lib/sokol-dll.c -lpthread -ldl -lm 
+    gcc -c -I./ lib/sokol-dll.c -o sokol_dll.so sokol-dll.o -lpthread -ldl -lm 
     ar rcs sokol-dll.so sokol-dll.o
 elif [ "${PLATFORM}" = "ios64" ]; then
-    gcc -c -I./ lib/sokol-dll.c -lpthread -ldl -lm -arch arm64
+    gcc -c -I./ lib/sokol-dll.c -o sokol_dll.so sokol-dll.o -lpthread -ldl -lm -arch arm64
     ar rcs sokol-dll.so sokol-dll.o
 elif [ "${PLATFORM}" = "android" ]; then
     export PATH="android-ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH"
     export PATH="android-ndk/toolchains/llvm/prebuilt/linux-x86_64/:$PATH"
     
-    aarch64-linux-android21-clang -c -I./ -Iandroid-ndk/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include lib/sokol-dll.c -D__ANDROID__ -D__ANDROID_MIN_SDK_VERSION__=21 -lpthread -lm -fPIC -static
-    llvm-ar rcs sokol-dll-aarch64.so sokol-dll.o
+    aarch64-linux-android21-clang -c -I./ -Iandroid-ndk/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include lib/sokol-dll.c -o sokol_dll.so sokol-dll.o -D__ANDROID__ -D__ANDROID_MIN_SDK_VERSION__=21 -lpthread -lm -fPIC -static
     rm sokol-dll.o
-    armv7a-linux-androideabi19-clang -c -I./ -Iandroid-ndk/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include lib/sokol-dll.c -D__ANDROID__ -D__ANDROID_MIN_SDK_VERSION__=19 -lpthread -lm -fPIC -static
-    llvm-ar rcs sokol-dll-armv7.so sokol-dll.o
+    armv7a-linux-androideabi19-clang -c -I./ -Iandroid-ndk/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include lib/sokol-dll.c -o sokol_dll.so sokol-dll.o -D__ANDROID__ -D__ANDROID_MIN_SDK_VERSION__=19 -lpthread -lm -fPIC -static
 fi
 
