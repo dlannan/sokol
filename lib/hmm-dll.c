8d9a23ea909522ecdc53f93d345d323cd2145861
David Lannan <dlannan68@gmail.com>
2024-10-05 11:28:19 +0930

    added implementation define

--- lib/hmm-dll.c
+++ lib/hmm-dll.c
@@ -1,6 +1,4 @@
-#define SOKOL_IMPL
-#define SOKOL_DLL
-#define SOKOL_NO_ENTRY
+#define HANDMADE_MATH_IMPLEMENTATION
 /* sokol 3D-API defines are provided by build options */
 #include "HandmadeMath.h"
 
