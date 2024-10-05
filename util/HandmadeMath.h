05f295cea0a1ee72ab759c4936e45ee174b04baf
David Lannan <dlannan68@gmail.com>
2024-10-05 14:07:45 +0930

    Added win32 exports for hmm.

--- util/HandmadeMath.h
+++ util/HandmadeMath.h
@@ -267,7 +267,7 @@ extern "C"
 #ifdef HANDMADE_MATH_STATIC
 #define HMMDEF static
 #else
-#ifdef defined(_WIN32)
+#ifdef defined(D_WINDLL)
 #define HMMDEF __declspec(dllexport)
 #else
 #define HMMDEF extern 
