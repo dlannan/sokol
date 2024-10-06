9868e67f7b5cdc8f34c80bd40d14e6d502229a9e
David Lannan <dlannan68@gmail.com>
2024-10-06 19:59:47 +1030

    Added win define for logging.

--- build-sokol.bat
+++ build-sokol.bat
@@ -1,5 +1,5 @@
 
-call cl.exe /I. /DSOKOL_GLCORE /D_USRDLL /D_WINDLL /Fe:bin\sokol_dll.dll lib\sokol-dll.c /link /DLL
-call cl.exe /I. /DSOKOL_GLCORE /D_USRDLL /D_WINDLL /Fe:bin\sokol_debug_dll.dll lib\sokol-debug-dll.c /link /DLL
+call cl.exe /I. /DSOKOL_GLCORE /D_USRDLL /D_WINDLL /D_SLOG_WINDOWS /Fe:bin\sokol_dll.dll lib\sokol-dll.c /link /DLL
+call cl.exe /I. /DSOKOL_GLCORE /D_USRDLL /D_WINDLL /D_SLOG_WINDOWS /Fe:bin\sokol_debug_dll.dll lib\sokol-debug-dll.c /link /DLL
 call cl.exe /I. /Iutil /DSOKOL_GLCORE /D_USRDLL /D_WINDLL /Fe:bin\sokol_shape_dll.dll lib\sokol-shape-dll.c /link /DLL
 call cl.exe /I. /Iutil /DSOKOL_GLCORE /D_USRDLL /D_WINDLL /Fe:bin\hmm_dll.dll lib\hmm-dll.c /link /DLL
