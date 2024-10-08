b184408343f45ba7147148424126b7d36bb372e1
David Lannan <dlannan68@gmail.com>
2024-10-08 23:03:09 +1030

    Added windows build for cimgui and imgui for sokol.

--- build-sokol.bat
+++ build-sokol.bat
@@ -1,6 +1,15 @@
-
 call cl.exe /I. /DSOKOL_GLCORE /D_WIN32 /D_USRDLL /D_WINDLL /D_SLOG_WINDOWS /Fe:bin\sokol_dll.dll lib\sokol-dll.c /link /DLL
 call cl.exe /I. /DSOKOL_GLCORE /D_WIN32 /D_USRDLL /D_WINDLL /D_SLOG_WINDOWS /Fe:bin\sokol_debug_dll.dll lib\sokol-debug-dll.c /link /DLL
 call cl.exe /I. /Iutil /DSOKOL_GLCORE /D_WIN32 /D_USRDLL /D_WINDLL /Fe:bin\sokol_shape_dll.dll lib\sokol-shape-dll.c /link /DLL
-call cl.exe /I. /Iutil /DSOKOL_GLCORE /D_WIN32 /D_USRDLL /D_WINDLL /Fe:bin\sokol_imgui_dll.dll lib\sokol-imgui-dll.c /link /DLL
+
+@REM First build imgui and cimgui
+call cl.exe /c /I. /Iutil /Ilib/imgui /DSOKOL_GLCORE /D_WIN32 /Fe:bin\imgui.obj lib\imgui_lib.cpp
+lib /nologo /out:imgui.lib imgui_lib.obj 
+call cl.exe /c /I. /Iutil /Ilib/cimgui /Ilib /Ilib/imgui /DSOKOL_GLCORE /D_WIN32 /Fe:bin\cimgui.obj lib\cimgui\cimgui.cpp lib\imgui_lib.cpp lib\cimgui_lib.cpp
+lib /nologo /out:cimgui.lib cimgui.obj 
+call cl.exe /I. /Ilib /Iutil /Ilib/cimgui /DSOKOL_GLCORE /D_WIN32 /D_USRDLL /D_WINDLL /Fe:bin\sokol_imgui_dll.dll lib\sokol-imgui-dll.c /link /DLL /LIBPATH:"." "imgui.lib" "cimgui.lib"
+
 call cl.exe /I. /Iutil /DSOKOL_GLCORE /D_WIN32 /D_USRDLL /D_WINDLL /Fe:bin\hmm_dll.dll lib\hmm-dll.c /link /DLL
+
+del *.obj
+del *.lib
--- lib/sokol-imgui-dll.c
+++ lib/sokol-imgui-dll.c
@@ -1,10 +1,16 @@
 #define SOKOL_IMPL
 #define SOKOL_DLL
-#define SOKOL_IMGUI_IMPL
 #define SOKOL_NO_ENTRY
 /* sokol 3D-API defines are provided by build options */
-#include "sokol_app.h"
+/* #include "sokol_app.h" */
+#define SOKOL_IMGUI_NO_SOKOL_APP
 #include "sokol_gfx.h"
 #include "sokol_log.h"
-#include "sokol_glue.h"
+/* #include "sokol_glue.h" */
+
+#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+#include "cimgui.h"
+
+#define SOKOL_IMGUI_IMPL
 #include "sokol_imgui.h"
+
