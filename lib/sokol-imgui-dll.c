a5969732f63d167e132c96eefd25c8ae7ab3c914
David Lannan <dlannan68@gmail.com>
2024-10-08 21:13:30 +1030

    Fixed lib for imgui.

--- lib/sokol-imgui-dll.c
+++ lib/sokol-imgui-dll.c
@@ -1,9 +1,10 @@
 #define SOKOL_IMPL
 #define SOKOL_DLL
+#define SOKOL_IMGUI_IMPL
 #define SOKOL_NO_ENTRY
 /* sokol 3D-API defines are provided by build options */
 #include "sokol_app.h"
 #include "sokol_gfx.h"
 #include "sokol_log.h"
 #include "sokol_glue.h"
-#include "sokol_gfx_imgui.h"
+#include "sokol_imgui.h"
