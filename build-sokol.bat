9690ec46a7fbb8c27115d1b5978cefb6213dbf4e
David Lannan <dlannan68@gmail.com>
2024-10-10 16:43:01 +1030

    Added nuklear

--- build-sokol.bat
+++ build-sokol.bat
@@ -2,7 +2,10 @@ call cl.exe /I. /DSOKOL_GLCORE /D_WIN32 /D_USRDLL /D_WINDLL /D_SLOG_WINDOWS /Fe:
 call cl.exe /I. /DSOKOL_GLCORE /D_WIN32 /D_USRDLL /D_WINDLL /D_SLOG_WINDOWS /Fe:bin\sokol_debug_dll.dll lib\sokol-debug-dll.c /link /DLL
 call cl.exe /I. /Iutil /DSOKOL_GLCORE /D_WIN32 /D_USRDLL /D_WINDLL /Fe:bin\sokol_shape_dll.dll lib\sokol-shape-dll.c /link /DLL
 
-@REM First build imgui and cimgui
+@REM Nuklear is the preferred GUI option. Much cleaner, and C friendly
+call cl.exe /I. /Iutil /Inuklear /DSOKOL_GLCORE /D_WIN32 /D_USRDLL /D_WINDLL /Fe:bin\sokol_nuklear_dll.dll lib\nuklear\nuklear.c lib\sokol-nuklear-dll.c /link /DLL
+
+@REM First build imgui and cimgui - DOnt use imgui. It is C++ without proper C bindings. Not worth the hassle (and its a mess too)
 call cl.exe /c /I. /Iutil /Ilib/imgui /DSOKOL_GLCORE /D_WIN32 /Fe:bin\imgui.obj lib\imgui_lib.cpp
 lib /nologo /out:imgui.lib imgui_lib.obj 
 call cl.exe /c /I. /Iutil /Ilib/cimgui /Ilib /Ilib/imgui /DSOKOL_GLCORE /D_WIN32 /Fe:bin\cimgui.obj lib\cimgui\cimgui.cpp lib\imgui_lib.cpp lib\cimgui_lib.cpp
--- lib/sokol-nuklear-dll.c
+++ lib/sokol-nuklear-dll.c
@@ -1,9 +1,21 @@
 #define SOKOL_IMPL
 #define SOKOL_DLL
 #define SOKOL_NO_ENTRY
+
 /* sokol 3D-API defines are provided by build options */
-#include "sokol_app.h"
 #include "sokol_gfx.h"
-#include "sokol_log.h"
-#include "sokol_glue.h"
+#include "sokol_app.h"
+
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+
+#include "nuklear.h"
+
+#define SOKOL_NUKLEAR_NO_SOKOL_APP
+#define SOKOL_IMGUI_IMPL
 #include "sokol_nuklear.h"
+
