c7eec91e0f4aa21cf0449690d01df00df3dab660
David Lannan <dlannan68@gmail.com>
2024-10-14 22:23:50 +1030

    Updated extern declarations

--- lib/stb-dll.c
+++ lib/stb-dll.c
@@ -1,3 +1,16 @@
+#if defined(_WIN32) 
+#define STBIDEF __declspec(dllimport)
+#define STBIRDEF __declspec(dllimport)
+#define STBIWDEF __declspec(dllimport)
+#define STBTT_DEF __declspec(dllimport)
+#else
+#define STBIDEF extern
+#define STBIRDEF extern
+#define STBIWDEF extern
+#define STBTT_DEF extern
+#endif
+
+
 /* stb files to make a simple stb lib */
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
@@ -7,5 +20,3 @@
 #include "stb/stb_image_resize2.h"
 #define STB_TRUETYPE_IMPLEMENTATION
 #include "stb/stb_truetype.h"
-#define STB_DS_IMPLEMENTATION
-#include "stb/stb_ds.h"
